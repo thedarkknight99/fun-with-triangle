78f80d012674b6e85d03fdd582a8179430076239
Abhishek <66775793+thedarkknight99@users.noreply.github.com>
2022-09-15 22:15:01 +0530

    fix: added innertext

--- Hypotenuse/hypotenuse.js
+++ Hypotenuse/hypotenuse.js
@@ -12,7 +12,7 @@ function calculateHypotenuse() {
         //console.log(Number(inputs[0].value), Number(inputs[1].value))
         const sumofSquares = calculateSUmOfSquares(Number(inputs[0].value), Number(inputs[1].value));
         const lengthHypotenuse = Math.sqrt(sumofSquares);
-        output.innerText = "Hypotenuse for given sides is " + lengthHypotenuse + "cm";
+        output.innerText = `Hypotenuse for given sides is ${lengthHypotenuse.toFixed(2)} cm`;
     }
     else if (Number(inputs[0].value) <= 0 || Number(inputs[1].value <= 0)) {
         output.innerText = "Sides of triangle cannot be negative or zero."
